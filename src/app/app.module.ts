472797f708416825e2483326b1f1ab51b15312fb
Brendan Cowley <b.cowley1220@gmail.com>
2019-09-03 07:07:28 -0400

    fitst commit, project ready to submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { QuizComponent } from "./quiz/quiz.component";
+import { ResultsComponent } from "./results/results.component";
+import { ScoresComponent } from "./scores/scores.component";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+import { QuizService } from "./services/quiz.service";
 
-import { AppComponent } from './app.component';
+let appRoutes: Routes = [
+  { path: "quiz", component: QuizComponent },
+  { path: "results", component: ResultsComponent },
+  { path: "scores", component: ScoresComponent },
+  { path: "", redirectTo: "/quiz", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    ResultsComponent,
+    ScoresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [QuizService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
